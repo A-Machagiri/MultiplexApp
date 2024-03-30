2176d3d2bf6f55eb0282bdb1ff9aacf7cadf27fa
A-Machagiri <adunurimachagiri@gmail.com>
2024-03-30 21:03:27 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,31 +1,61 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js'
-import cors from 'cors';
-//Configure env
-dotenv.config()
+import express from "express";
+import colors from "colors";
+import morgan from "morgan";
+import Userdata from "./models/userModel.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
 
-//Database 
-connectDB()
-// rest object
-const app=express();
+connectDB();
 
-//middlewares 
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'));
+const app = express();
 
-//Routes
-app.use('/api/v1/auth',authRoutes);
-//rest api
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-
-//PORT
-
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
-  console.log(`Serving in mode on ${process.env.DEV_MODE} running on port http://localhost:${PORT}`.bgCyan.white);
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, phone, address, role } = req.body;
+    const newUser = new Userdata({
+      name,
+      email,
+      password,
+      phone,
+      address,
+      role,
+    });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Error registering user" });
+  }
+});
+app.post("/login",(req,res)=>{
+  const{email,password}=req.body;
+  Userdata.findOne({email:email})
+  .then(user=>{
+    if(user){
+      if(user.password===password){
+        res.json("Success");
+      }else{
+        res.json("The password is inCorrect")
+      }
+    }else{
+      res.json("No Record existed");
+    }
+  })
+  .catch(error=>{
+    console.error(error);
+    res.status(500).json("An error occurred during login");
+  })
 })
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(
+    `Serving in mode on running on port http://localhost:${PORT}`.bgCyan.white
+  );
+});
